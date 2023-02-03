92bcdb15512f3b6eedbd204c66a7faec4c1fd547
Khabibullokh18022004 <93874103+Khabibullokh18022004@users.noreply.github.com>
2023-02-03 20:12:58 +0500

    My second commit

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,19 @@
+// Khabibullokh Khallokov
+// Practise in IOS Swift UIKit
+import UIKit
+
+class ViewController: UIViewController {
+    //IBOutlet allows refer our image
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var DiceImageView2: UIImageView!
+
+     
+    @IBAction func rollButtonPressed(_ sender: UIButton)
+    {
+        let diceArray = ["DiceOne", "DiceTwo","DiceThree", "DiceFour","DiceFive", "DiceSix" ]
+        diceImageView1.image = UIImage(named: diceArray.randomElement()!)
+        DiceImageView2.image = UIImage(named : diceArray.randomElement()!)
+        
+    }
+}
+ 
